19a1b131a971005652e58d42ce905b3383cba3b7
CLiF <clif1593@gmail.com>
2023-05-20 03:27:03 +0900

    Separate Sorting Test Code

---
+++ SortingAlgorithm/Sorting_Test.py
@@ -0,0 +1,21 @@
+from SortingAlgorithm.Sort import ArrayType
+from SortingAlgorithm.Bubble_Sort import BubbleSort
+from SortingAlgorithm.Insertion_Sort import InsertionSort
+from SortingAlgorithm.Selection_Sort import SelectionSort
+from SortingAlgorithm.Quick_Sort import QuickSort
+from SortingAlgorithm.Merge_Sort import MergeSort
+from SortingAlgorithm.Shell_Sort import ShellSort
+from SortingAlgorithm.Radix_Sort import RadixSort
+from SortingAlgorithm.Heap_Sort import HeapSort
+from SortingAlgorithm.CombSort import CombSort
+
+import time
+
+if __name__ == '__main__':
+    sort = CombSort()
+    sort.SetArray(100, 1, ArrayType.RANDOM)
+    sort.SetSpeed(0.02)
+    sort.SetPlaysound(True)
+    sort.Sort()
+    while not sort.IsFinished():
+        time.sleep(0.016)
